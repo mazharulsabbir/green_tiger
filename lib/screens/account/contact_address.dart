ea355625d330c7e9ef96dab53904f45e6081217d
mazharulsabbir <mazharulsabbir2@gmail.com>
2022-04-19 22:50:47 +0600

    contact and address design update

--- lib/screens/account/contact_address.dart
+++ lib/screens/account/contact_address.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:get/get.dart';
 import 'package:green_tiger/controller/user/contact_address_controller.dart';
+
+import '../../constraints/index.dart';
 
 class ContactAndAddressScreen extends GetView<ContactAndAddressController> {
   const ContactAndAddressScreen({Key? key}) : super(key: key);
@@ -11,11 +14,60 @@ class ContactAndAddressScreen extends GetView<ContactAndAddressController> {
       appBar: AppBar(title: const Text('Contact and Address')),
       body: controller.obx(
         (state) => ListView.builder(
+          padding: const EdgeInsets.only(top: 8),
           itemCount: state?.length,
-          itemBuilder: ((context, index) => ListTile(
-                title: Text('${state?[index].name}'),
-                subtitle: Text('${state?[index].phone}'),
-              )),
+          itemBuilder: (context, index) => Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: primaryColor),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      '${state?[index].name}',
+                      style: Theme.of(context).textTheme.headline6?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        InkWell(
+                          child: Row(
+                            children: const [
+                              Text(
+                                'EDIT',
+                              ),
+                              SizedBox(width: 5),
+                              Icon(
+                                PhosphorIcons.pencil,
+                                size: 16,
+                              ),
+                            ],
+                          ),
+                          // onTap: () => Get.to(() => AddressFormWidget()),
+                        ),
+                        const SizedBox(width: 15),
+                        const InkWell(
+                          child: Icon(PhosphorIcons.trash),
+                          onTap: null,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text('${state?[index].street}'),
+                const SizedBox(height: 20),
+                Text('${state?[index].phone}'),
+              ],
+            ),
+          ),
         ),
       ),
     );
