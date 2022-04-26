c80bf5726bc4fc8efa8dab0594290aaa36870f85
Mazharul Sabbir <mazharulsabbir2@gmail.com>
2022-04-26 23:58:04 +0600

    get products by category

--- lib/screens/product/product_by_category.dart
+++ lib/screens/product/product_by_category.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:get/get.dart';
+import 'package:green_tiger/data/model/category/category.dart';
 
+import '../../controller/product/category_controller.dart';
 import '../../controller/product/product_by_category_controller.dart';
-import 'widget/product_list_tile_widget.dart';
 import 'widget/product_widget.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -13,7 +15,39 @@ class ProductByCategoryScreen extends GetView<ProductByCategoryController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${controller.category?.name}'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: DropdownButtonFormField<CategoryModel>(
+          isExpanded: true,
+          items: List.generate(
+            CategoryController.to.state?.length ?? 0,
+            (index) => DropdownMenuItem(
+              child: Text(CategoryController.to.state?[index].name ?? ''),
+              value: CategoryController.to.state?[index],
+            ),
+          ),
+          onChanged: (value) {
+            ProductByCategoryController.to.getProductByCategory(value!);
+          },
+          value: ProductByCategoryController.to.category,
+          decoration: const InputDecoration(border: InputBorder.none),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              PhosphorIcons.sort_ascending,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              PhosphorIcons.funnel,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: controller.obx(
         (state) => SingleChildScrollView(
